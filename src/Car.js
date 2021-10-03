82c133a496fd5c1259e19028651e42bfaee04536
maribelcuales <maribelcuales@gmail.com>
2021-10-03 12:34:49 +0800

    Create a constructor function in the Car component and added color property.

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,22 @@
-function Car() {
-  return <h2>Hi, I am a Car!</h2>
+import React from 'react';
+
+// Create a constructor function and add a color property:
+class Car extends React.Component { 
+  constructor() {
+    super();
+    this.state = {color: "red"};
+  }
+  render() {
+    return <h2>I am a {this.state.color} Car!</h2>;
+  }
 }
 
 export default Car;
+
+
+/**
+function Car() {
+  return <h2>Hi, I am a Car!</h2>
+
+export default Car;
+**/
