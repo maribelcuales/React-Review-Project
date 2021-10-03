abc474ccfdcb280bdef5e181454784b5b1ec30f5
maribelcuales <maribelcuales@gmail.com>
2021-10-03 12:44:26 +0800

    Comments added to Car class component.

--- src/Car.js
+++ src/Car.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-// Create a constructor function and add a color property:
-class Car extends React.Component { 
+// Create a Class Component with a constructor function and add a color property
+
+// extends React.Component creates an inheritance for this component giving it access to React.Component's functions     
+class Car extends React.Component {
+  // constructor() is called when a component is initiated; also initiates the component's properties 
   constructor() {
+    // super() executes the parent component's constructor function allowing this component access to all the parent component's functions  
     super();
+    // component properties are kept in an object called state   
     this.state = {color: "red"};
   }
+  // render method returns HTML 
   render() {
     return <h2>I am a {this.state.color} Car!</h2>;
   }
