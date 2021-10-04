f298b860634c9f91b1293428ff52eee3843b718c
maribelcuales <maribelcuales@gmail.com>
2021-10-04 18:03:08 +0800

    Added changeColor function and button to Header component.

--- src/Header.js
+++ src/Header.js
@@ -8,9 +8,15 @@ class Header extends React.Component {
   static getDerivedStateFromProps(props, state) {
     return {favoriteColor: props.favCol};
   }
+  changeColor = () => {
+    this.setState({favoriteColor: "blue"});
+  }
   render() {
     return (
+      <div>
       <h1>My Favorite Color is {this.state.favoriteColor}</h1>
+      <button type="button" onClick={this.changeColor}Change Color></button>
+      </div>
     );
   }
 }
