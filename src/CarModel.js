3d53605399b1c7da1ca7c349e320fd702d086596
maribelcuales <maribelcuales@gmail.com>
2021-10-04 18:37:09 +0800

    Update CarModel component with shouldComponentUpdate returning false.

--- src/CarModel.js
+++ src/CarModel.js
@@ -3,9 +3,21 @@ import React from 'react';
 class CarModel extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {favoriteModel:"Ferrari"}
+  }
+  shouldComponentUpdate(){
+    return false;
+  }
+  changeColor = () => {
+    this.setState({favoriteModel: "Lamborgini"});
   }
   render() {
-    return <h2>I am a {this.props.model} car!</h2>
+    return (
+    <div>
+      <h1>My Favorite Model is {this.state.favoriteModel}</h1>
+      <button type="button" onClick={this.changeColor}>Change Model</button>
+    </div>
+    );
   }
 }
 
