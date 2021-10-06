b875d3f8c411fc85e7db6623a9d69dd5762efc47
maribelcuales <maribelcuales@gmail.com>
2021-10-07 05:02:46 +0800

    Added carInfo object passing it as props to Car component.

--- src/CarName.js
+++ src/CarName.js
@@ -1,13 +1,19 @@
 function Car(props) {
-  return <h2>I am a {props.brand}!</h2>;
+  // return <h2>I am a {props.brand}!</h2>;
+  return <h2>I am a {props.brand.model}!</h2>
 }
 
 function CarName() {
-  const carModel = "Ford";
+  //const carModel = "Ford";
+  // Create a carInfo object 
+  const carInfo = {
+    name: "Ford",
+    model: "Mustang"
+  };
   return (
     <div>
       <h1>Who lives in my garage?</h1>
-      <Car brand={carModel} />
+      <Car brand={carInfo} />
     </div>
   );
 }
