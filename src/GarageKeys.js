113c24f0aaf6754649f7745905f9fafcc8c322cb
maribelcuales <maribelcuales@gmail.com>
2021-10-08 03:18:55 +0800

    Created GarageKeys component mapping over an array of cars and then rendered it to root.

---
+++ src/GarageKeys.js
@@ -0,0 +1,21 @@
+function Car(props) {
+  return <li>I am {props.brand}</li>
+}
+
+function GarageKeys() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <div>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
+    </div>
+  );
+}
+
+export default GarageKeys;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Garage from './Garage.js';
+import GarageKeys from './GarageKeys.js';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
@@ -11,6 +11,6 @@ ReactDOM.render(
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
   //<GoalType isGoal={true} />,
-  <Garage />,
+  <GarageKeys />,
   document.getElementById('root')
 ); 
