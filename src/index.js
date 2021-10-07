12e9601e90810393bb6f58530f55e67a78dc34a8
maribelcuales <maribelcuales@gmail.com>
2021-10-08 03:43:41 +0800

    Added MyForm component and render it to root.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import GarageKeys from './GarageKeys.js';
+import MyForm from './MyForm.js';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
@@ -11,6 +11,6 @@ ReactDOM.render(
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
   //<GoalType isGoal={true} />,
-  <GarageKeys />,
+  <MyForm />,
   document.getElementById('root')
 ); 
