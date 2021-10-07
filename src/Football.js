6f623c276cd4346668a378e7269670d141fcfeb4
maribelcuales <maribelcuales@gmail.com>
2021-10-07 18:21:51 +0800

    Refactored to send Goal! as a parameter to the shoot function.

--- src/Football.js
+++ src/Football.js
@@ -1,9 +1,16 @@
 function Football() {
-  const shoot = () => {
-    alert("Great Shot!");
+  //const shoot = () => {
+  //  alert("Great Shot!");
+
+  const shoot = (a) => {
+    alert(a);
   }
+
   return (
-    <button onClick={shoot}>Take the shot!</button>
+    //<button onClick={shoot}>Take the shot!</button>
+    
+    // send "Goal!" as a parameter to the shoot function  
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
   );
 }
 
