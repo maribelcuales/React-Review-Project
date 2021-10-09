41762befb2083b4377b9e96d6b4f3c9a6e49538d
maribelcuales <maribelcuales@gmail.com>
2021-10-09 14:41:09 +0800

    Render App.js to the root element.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Contact from './pages/Contact';
-import './App.css';
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import MyFormSelect from './MyFormSelect.js';
+import App from './App';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
 ReactDOM.render(
-  //<App />, 
+  <App />, 
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
   //<GoalType isGoal={true} />,
-  <MyFormSelect />,
   document.getElementById('root')
 ); 
