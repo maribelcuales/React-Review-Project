c598eff2585fe482000eaf0c5703156869516b80
maribelcuales <maribelcuales@gmail.com>
2021-10-10 16:27:53 +0800

    Create and import stylesheet module to Plane component.

--- src/Plane/Plane.js
+++ src/Plane/Plane.js
@@ -1,5 +1,7 @@
+import styles from './my-style-module.css';
+
 const Plane = () => {
-  return <h1>It's a plane!</h1>
+  return <h1 className={styles.bigblue}>It's a plane!</h1>;
 }
 
 export default Plane;
