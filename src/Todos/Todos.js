60d19bde4fa569702b998fff23766dc83cb0c251
maribelcuales <maribelcuales@gmail.com>
2021-10-10 15:19:17 +0800

    Created a style object headerStyle in Todos component.

--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -2,9 +2,17 @@ import { memo } from 'react';
 
 const Todos = ({todos}) => {
   console.log("child render");
+
+  const headerStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans Serif"
+  };
+
   return (
     <>
-      <h2>My Todos</h2>
+      <h2 style={headerStyle}>My Todos</h2>
       {todos.map((todo, index) => {
         return <p key={index}>{todo}</p>
       })};
