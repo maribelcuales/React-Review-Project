786d4f3397b1c4fe8761410329d83d3bb72aa707
maribelcuales <maribelcuales@gmail.com>
2021-10-10 16:31:25 +0800

    Fixed typo error in the import of the stylesheet module.

--- src/Plane/Plane.js
+++ src/Plane/Plane.js
@@ -1,4 +1,4 @@
-import styles from './my-style-module.css';
+import styles from './my-style.module.css';
 
 const Plane = () => {
   return <h1 className={styles.bigblue}>It's a plane!</h1>;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import TodoApp from './Todos/TodoApp.js';
+//import TodoApp from './Todos/TodoApp.js';
+import Plane from './Plane/Plane.js';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
 ReactDOM.render(
-  <TodoApp />,
+  <Plane />,
   //<App />, 
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
