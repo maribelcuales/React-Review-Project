3a736597b8c1be2f1322cbf1d6195faf558d8ab0
maribelcuales <maribelcuales@gmail.com>
2021-10-11 12:45:26 +0800

    Created Timer component and render it to root.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 //import TodoApp from './Todos/TodoApp.js';
-import CarObject from './Car/CarObject.js';
+import Timer from './Timer/Timer.js';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
 ReactDOM.render(
-  <CarObject />,
+  <Timer />,
   //<App />, 
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
