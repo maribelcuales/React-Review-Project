6e8403e338218b3cd1394fa096dd8d535415e5c4
maribelcuales <maribelcuales@gmail.com>
2021-10-11 14:52:50 +0800

    Created Component 4.

---
+++ src/Component/Component3.js
@@ -0,0 +1,10 @@
+import Component4 from "./Component4.js";
+
+export default function Component3({user}) {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 user={user}/>
+    </>
+  );
+}
