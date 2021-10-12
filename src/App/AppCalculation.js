266684472a7d5f82cfb87074422d98d7bbad2541
maribelcuales <maribelcuales@gmail.com>
2021-10-12 17:09:19 +0800

    Wrapped the expensive function call with useMemo.

--- src/App/AppCalculation.js
+++ src/App/AppCalculation.js
@@ -1,11 +1,14 @@
-// When changing the count or adding a todo, you will notice a delay in execution because of a poor performing function 
+// At first, you will notice a delay in execution when changing the count or adding a todo, because of a poor performing function  
+// Solution -> wrap the expensive function call with useMemo 
+// This way the function will only run when its dependencies have changed  
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const AppCalculation = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-  const calculation = expensiveCalculation(count);
+  // wrap the expensive function call with useMemo 
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
   const increment = () => {
     setCount((c) => c + 1);
@@ -35,7 +38,9 @@ const AppCalculation = () => {
   );
 };
 
-// This is a poor performing function because it runs on every render 
+// This was a poor performing function because it runs on every render 
+// Solved! -> by wrapping the expensive function call with useMemo 
+// This way the function will only run when its dependencies have changed  
 const expensiveCalculation = (num) => {
   console.log("Calculating...");
   for (let i = 0; i < 1000000000; i++) {
