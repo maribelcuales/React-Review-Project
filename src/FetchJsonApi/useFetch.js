79b17fd6e16decfe56fc2b97bd9cacb5f5ae9b55
maribelcuales <maribelcuales@gmail.com>
2021-10-12 19:15:31 +0800

    Moved the fetch logic to be used as a custom hook.

---
+++ src/FetchJsonApi/useFetch.js
@@ -0,0 +1,16 @@
+// moved the fetch logic here to be used as a custom hook 
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [url]);
+
+  return [data];
+};
+
+export default useFetch; 
