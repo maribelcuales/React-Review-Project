ef88bd2cdff6bd78d6a89c94162888e3ce272087
maribelcuales <maribelcuales@gmail.com>
2021-10-12 15:53:31 +0800

    Added useCallback to prevent Todos component from re-rendering needlessly.

--- src/Todos/AppCallback.js
+++ src/Todos/AppCallback.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import TodoCallback from "./TodoCallback.js";
 
 const AppCallback = () => {
@@ -9,9 +9,9 @@ const AppCallback = () => {
     setCount((c) => c + 1);
   };
 
-  const addTodo = () => {
+  const addTodo = useCallback(() => {
     setTodos((t) => [...t, "New Todo"]);
-  };
+  }, [todos]);
 
   return (
     <>
