ef1e05fee68c6f6ec10b5673d95abe0c25f66198
maribelcuales <maribelcuales@gmail.com>
2021-10-12 13:53:44 +0800

    Created TodoCounter component using useReducer hook in a counter app.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 //import TodoApp from './Todos/TodoApp.js';
-import AppPrevState from './App/AppPrevState.js';
+import TodoCounter from './Todos/TodoCounter.js';
 
 //const cars = ['Ford', 'BMW', 'Audi'];
 
 ReactDOM.render(
-<AppPrevState />,
+<TodoCounter />,
   //<App />, 
   //<Header favCol="yellow"/>,
   //<GarageCars cars={cars}/>,
