532834bf6058b918990de6182246ac276fadc507
maribelcuales <maribelcuales@gmail.com>
2021-10-12 14:00:15 +0800

    Created AppCallback component.

---
+++ src/Todos/AppCallback.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import TodoCallback from "./TodoCallback.js";
+
+const AppCallback = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <>
+      <TodoCallback todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default AppCallback;
